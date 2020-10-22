7a65772424db7b4016a4b78190d47ca5fc301541
FynnKaeser <fynn.kaeser@icloud.com>
2020-10-22 10:58:32 +0200

    Added BookingsController Added POST Method for Bookings

---
+++ src/biz.dfch.CS.SampleIPA.StockManagement.API/Controllers/BookingsController.cs
@@ -0,0 +1,46 @@
+﻿using biz.dfch.CS.SampleIPA.StockManagement.API.Data;
+using biz.dfch.CS.SampleIPA.StockManagement.API.Models;
+using Microsoft.AspNet.OData;
+using Microsoft.AspNet.OData.Routing;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace biz.dfch.CS.SampleIPA.StockManagement.API.Controllers
+{
+    [ODataRoutePrefix(nameof(Booking))]
+    public class BookingsController : ODataController
+    {
+        private readonly StockManagementContext _context;
+
+        public BookingsController(StockManagementContext context)
+        {
+            _context = context;
+
+            if (!context.Bookings.Any())
+            {
+                _context.Database.EnsureCreated();
+            }
+        }
+
+        [ODataRoute]
+        [HttpPost]
+        public IActionResult CreateBooking([FromBody] BookingDto bookingDto)
+        {
+            var product = _context.Products.Find(bookingDto.ProductId);
+            var booking = new Booking
+            {
+                Amount = bookingDto.Amount,
+                DataTime = new DateTime(),
+                Product = product
+            };
+
+            _context.Bookings.Add(booking);
+            _context.SaveChanges();
+
+            return Created(booking);
+        }
+    }
+}
---
+++ src/biz.dfch.CS.SampleIPA.StockManagement.API/Models/BookingDto.cs
@@ -0,0 +1,8 @@
+﻿namespace biz.dfch.CS.SampleIPA.StockManagement.API.Models
+{
+    public class BookingDto
+    {
+        public int Amount { get; set; }
+        public int ProductId { get; set; }
+    }
+}
